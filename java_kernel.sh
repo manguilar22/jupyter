4c42ec3c0f1af3a83a183fb76a7dab6d25388a31
user <manguilar22@gmail.com>
2019-05-30 16:50:27 -0600

    ...@Thu May 30 16:50:27 MDT 2019

--- java_kernel.sh
+++ java_kernel.sh
@@ -1,19 +1,24 @@
 #!/bin/bash 
 
-# Get Java Kernel for Jupyter 
-
+# Java Kernel for Jupyter (Version) 
 kernel="ijava-1.3.0.zip"
 
+# Download Java Kernel
 wget https://github.com/SpencerPark/IJava/releases/download/v1.3.0/$kernel
 
+# Unzip Java Kernel zip 
 unzip $kernel 
 
+# Move installation files 
 mv $kernel /tmp 
 
 mv install.py /tmp 
 
+# You need python3 
+# Python3.x >= 3.0
 python /tmp/install.py --sys-prefix 
 
+# List Kernels Installed 
 echo "\nCheck if java kernel is succesfully installed.\n\n" 
 
 jupyter kernelspec list
